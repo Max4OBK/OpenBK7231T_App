d3019efd850cd889d2a11651dfded39904fee8cd
Max4OBK <MaxMuster@vorsicht-bissig.de>
2024-10-24 14:10:58 +0200

    Update pre_build.sh for Moes Matter
    in    [BK_PARTITION_RF_FIRMWARE]
    
    changed
    
    .partition_start_addr      = 0x1D0000,
    to
    .partition_start_addr      = 0x1E3000,

--- platforms/BK7231N/pre_build.sh
+++ platforms/BK7231N/pre_build.sh
@@ -1,9 +1,9 @@
 FILE=sdk/OpenBK7231N/platforms/bk7231n/bk7231n_os/beken378/func/user_driver/BkDriverFlash.c
 # show content before sed
 echo "BK_PARTITION_RF_FIRMWARE before prebuild.sh:"
-grep - A 6 BK_PARTITION_RF_FIRMWARE $FILE
+grep -A 6 BK_PARTITION_RF_FIRMWARE $FILE
 # do the sed
 sed -i "s/0x1D0000,/0x1E3000,/" $FILE
 # show content after sed
 echo "BK_PARTITION_RF_FIRMWARE after change from prebuild.sh:"
-grep - A 6 BK_PARTITION_RF_FIRMWARE $FILE
+grep -A 6 BK_PARTITION_RF_FIRMWARE $FILE
